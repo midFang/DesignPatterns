655c1d1bc1c245aae1d9d141dbfee212be94bfbf
fangsf <highestfeng@gmail.com>
2018-11-08 14:23:35 +0800

    activity的管理类

--- app/src/main/java/example/fangsf/desiginpatterns/singleton/SingletonActivity.java
+++ app/src/main/java/example/fangsf/desiginpatterns/singleton/SingletonActivity.java
@@ -12,7 +12,6 @@ public class SingletonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleton);
 
-
         SingleSample1.getInstance().println();
 
 
